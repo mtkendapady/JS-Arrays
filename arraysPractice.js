607e926933efa32948a4fa0a0fb92ef9d98e851e
Matangi Kendapady <mtkendapady@gmail.com>
2016-09-01 20:55:53 -0500

    practice arrays

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,12 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
-  //Code Here
+var arr = [10,20,30];
+function first() {
+  console.log(arr[0]);
+}
+
+first(arr);
 
 
 //Next problem
@@ -12,64 +17,121 @@ var arr = [10,20,30];
 
 
 var arr = [40,50,60];
+
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
 
+var arr = [40,50,60];
+function last() {
+  console.log(arr[arr.length-1]);
+}
 
-  //Code Here
+last(arr);
+
 
 
 //Next Problem
 
 
-var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
-//Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+//Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
+var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
+function looper() {
+  console.log( family );
+}
+
+looper();
 
 
 //Next problem
 
 
 
-var letters = ['A', 'B', 'C', 'D', 'E'];
+
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
-  //Code Here
+var letters = ['A', 'B', 'C', 'D', 'E'];
+function reversedLooper(){
+  console.log( letters.reverse() );
+}
+
+reversedLooper(letters);
+
 
 
 //Next Problem
 
 
-var nums = [1,2,3,6,22,98,45,23,22,12];
+
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
-  //Code Here
+var nums = [1,2,3,6,22,98,45,23,22,12];
+function evenFinder() {
+  var evens = [];
+  for (var i = 0; i < nums.length; i++) {
+    if ((nums[i] % 2) === 0) {
+      evens.push(nums[i]);
+    }
+  }
+  console.log(evens);
+}
+
+evenFinder(nums);
 
 
 //Next problem
 
 
-var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 //Write a function called divider that is given one argument, numbersArray.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
+var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+function divider(numbersArray) {
+  var evens = [];
+  var odds = [];
+  var both = [];
 
+   for (var i = 0; i < numbersArray.length; i++) {
+    if ((numbersArray[i] % 2) === 0) {
+      evens.push(numbersArray[i]);
+    } else {
+      odds.push(numbersArray[i]);
+    }
+  }
+  both.push(evens, odds);
+  return both;
 
-  //Code Here
+}
+
+divider(numbersArray);
 
 
 //Next Problem
 
 
-var getRandomArbitrary = function() {
-  return Math.floor(Math.random() * (30 - 0) + 0);
-};
+
 // var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
+var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+var getRandomArbitrary = function() {
+  return Math.floor(Math.random() * (30 - 0) + 0);
+};
+function finder() {
+  var random = getRandomArbitrary();
+  var a = numbers.indexOf(random);
+    if ( a === -1) {
+      return false;
+    } else {
+      return true;
+    }
+
+    //return  numbers.indexOf( random ) !== -1; THIS IS ANOTHER CLEANER WAY OF WRITING IT.
+
+}
+
+finder(numbers);
 
   //Code Here
 
@@ -77,12 +139,20 @@ var getRandomArbitrary = function() {
 //Next problem
 
 
-
-var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
+var str = 'this is my sentence';
+function reverse() {
+  var arrayStr = str.split(" ");
+  console.log( arrayStr );
+  var newStr = arrayStr.reverse();
+  console.log( newStr );
+  var revStr = newStr.join(" ");
+  console.log( revStr );
 
-  //Code Here
+  //return str.split( " " ).reverse().join(); THIS IS ANOTHER CLEANER WAY OF WRITING IT.
+}
 
+reverse(str);
 
 //Next Problem
 
@@ -91,15 +161,12 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 /*
   Here we're going to write a function that mimics going shopping and checking things off of our grocery list,
   and adding new items to our list.
-
   Write a function called removeItem that is given two arguments, the first is myGroceryList, and the
   second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList,
   remove that item from the your grocery list and return the new, updated grocery list.
-
   Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList
   and the second is an item to add to your grocery list. In addItem add the item you passed in to
   myGroceryList then return the new, updated grocery list.
-
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
